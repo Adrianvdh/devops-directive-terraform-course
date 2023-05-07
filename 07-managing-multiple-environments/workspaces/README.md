81353ce94a1e807d048e6cc20e3919381813fc53
Adrian van den Houten <adrianvdh@gmail.com>
2023-05-07 23:16:22 +0200

    Update project

--- 07-managing-multiple-environments/workspaces/README.md
+++ 07-managing-multiple-environments/workspaces/README.md
@@ -2,5 +2,7 @@ Warning about manually switching environments
 ```
 terraform workspace new production
 terraform workspace list
+terraform workspace select production
 terraform workspace select staging
+terraform destroy -lock=false
 ```
--- 07-managing-multiple-environments/workspaces/main.tf
+++ 07-managing-multiple-environments/workspaces/main.tf
@@ -2,9 +2,9 @@ terraform {
   # Assumes s3 bucket and dynamo DB table already set up
   # See /code/03-basics/aws-backend
   backend "s3" {
-    bucket         = "devops-directive-tf-state"
+    bucket         = "devops-directive-terraform-state" # REPLACE WITH YOUR BUCKET NAME
     key            = "07-managing-multiple-environments/workspaces/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "af-south-1"
     dynamodb_table = "terraform-state-locking"
     encrypt        = true
   }
@@ -18,14 +18,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "af-south-1"
 }
 
-variable "db_pass" {
-  description = "password for database"
-  type        = string
-  sensitive   = true
-}
+# variable "db_pass" {
+#   description = "password for database"
+#   type        = string
+#   sensitive   = true
+# }
 
 locals {
   environment_name = terraform.workspace
@@ -35,12 +35,15 @@ module "web_app" {
   source = "../../06-organization-and-modules/web-app-module"
 
   # Input Variables
+  region           = "af-south-1"
   bucket_prefix    = "web-app-data-${local.environment_name}"
   domain           = "devopsdeployed.com"
   environment_name = local.environment_name
-  instance_type    = "t2.micro"
+  instance_type    = "t3.small"
   create_dns_zone  = terraform.workspace == "production" ? true : false
   db_name          = "${local.environment_name}mydb"
   db_user          = "foo"
-  db_pass          = var.db_pass
+  # db_pass          = var.db_pass
+  db_pass = "SuperSecurePassword"
+  db_instance_type = "db.t3.small"
 }
